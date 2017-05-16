584798684e1456e11806a32466acef5221506b18
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-05-16 16:58:39 +0300

    first red e2e test

--- test/browser/app.e2e.js
+++ test/browser/app.e2e.js
@@ -1,11 +1,21 @@
 import {expect} from 'chai';
 import 'babel-polyfill';
 
+const appDriver = {
+  navigate: (url = '/') => browser.get(url),
+  clickNewGame: () => $(`[data-hook=new-game]`).click()
+};
+
+const gameDriver = {
+  getRows: () => $$('[data-hook=row]'),
+  getCells: () => $$('[data-hook=cell]')
+};
+
 describe('React application', () => {
-  describe('open page', () => {
-    it('should display title', async () => {
-      await browser.get('/');
-      expect(await $('h2').getText()).to.eql('Hello World!');
-    });
+  it('should create a new game', async () => {
+    await appDriver.navigate();
+    await appDriver.clickNewGame();
+    expect(await gameDriver.getRows().count()).to.eql(10);
+    expect(await gameDriver.getCells().count()).to.eql(100);
   });
 });
