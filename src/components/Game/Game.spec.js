3a696e693ff1664c3d5afa9ef69224c17c31a440
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-05-18 15:02:40 +0300

    add a red component test for saving a game

---
+++ src/components/Game/Game.spec.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {expect} from 'chai';
+import nock from 'nock';
+import {mount} from 'enzyme';
+import Game from './Game';
+import {getTestBaseUrl} from '../../../test/test-common';
+import eventually from 'wix-eventually';
+
+const driver = {
+  saveGame: ({wrapper, gameName}) => {
+    wrapper.find('[data-hook="game-name-input"]').simulate('change', {target: {value: gameName}});
+    wrapper.find('[data-hook="game-save"]').simulate('click');
+  }
+};
+describe('Game', () => {
+  let wrapper;
+
+  afterEach(() => {
+    if (!nock.isDone()) {
+      throw new Error(`pending mocks: ${nock.pendingMocks()}`);
+    }
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  afterEach(() => wrapper.detach());
+
+  it('should save a game', () => {
+    let savedData;
+    const gameData = {name: 'Yaniv', board: {}};
+    nock(getTestBaseUrl())
+      .post('/api/game', body => {
+        savedData = body;
+        return true;
+      }).reply(200);
+
+    wrapper = mount(
+      <Game/>,
+       {attachTo: document.createElement('div')}
+    );
+
+    driver.saveGame({wrapper, gameName: gameData.name});
+    return eventually(() => expect(savedData).to.eql(gameData));
+  });
+});
