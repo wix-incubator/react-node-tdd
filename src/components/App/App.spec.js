7f9d29e4bc9a38f8b46ddea0dbb8b6e4e9300b0d
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-05-19 09:48:28 +0300

    add a red component test for loading saved games

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,18 +1,46 @@
-// import 'jsdom-global/register';
-// import React from 'react';
-// import {expect} from 'chai';
-// import {mount} from 'enzyme';
-// import App from './App';
+import React from 'react';
+import {expect} from 'chai';
+import {mount} from 'enzyme';
+import nock from 'nock';
+import tp from 'trier-promise';
+import {StaticRouter} from 'react-router';
+import App from './App';
+import {getTestBaseUrl} from '../../../test/test-common';
 
-// describe('App', () => {
-//   let wrapper;
+describe('App', () => {
+  let wrapper;
 
-//   afterEach(() => wrapper.detach());
+  afterEach(() => {
+    if (!nock.isDone()) {
+      throw new Error(`pending mocks: ${nock.pendingMocks()}`);
+    }
+  });
 
-//   it('renders a title correctly', () => {
-//     wrapper = mount(
-//       <App/>, {attachTo: document.createElement('div')}
-//     );
-//     expect(wrapper.find('h2').length).to.eq(1);
-//   });
-// });
+  afterEach(() => {
+    nock.cleanAll();
+  });
+  afterEach(() => wrapper.detach());
+
+  it('should load saved games', () => {
+    const games = [{name: 'yaniv', board: {}}];
+    const context = {};
+    nock(getTestBaseUrl()).get('/api/games').reply(() => games);
+
+    wrapper = mount(
+      <StaticRouter context={context}>
+        <App/>
+      </StaticRouter>, {attachTo: document.createElement('div')}
+    );
+
+    return eventually(() =>
+      expect(wrapper.find('[data-hook="game"]').text()).to.eql(games[0].name));
+  });
+});
+
+function eventually(action) {
+  return tp({
+    action,
+    timeout: 10000,
+    interval: 200
+  });
+}
