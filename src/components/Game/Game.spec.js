72d40e2dbeb00c6431572f591b22cc1ae8a4f0e9
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-05-21 14:36:30 +0300

    add a (red) component test for loading a saved game

--- src/components/Game/Game.spec.js
+++ src/components/Game/Game.spec.js
@@ -3,6 +3,8 @@ import {expect} from 'chai';
 import nock from 'nock';
 import {mount} from 'enzyme';
 import Game from './Game';
+import {MemoryRouter} from 'react-router';
+import {Route} from 'react-router-dom';
 import {getTestBaseUrl, eventually} from '../../../test/test-common';
 
 const driver = {
@@ -10,7 +12,8 @@ const driver = {
     wrapper.find('[data-hook="game-name-input"]').simulate('change', {target: {value: gameName}});
     wrapper.find('[data-hook="game-save"]').simulate('click');
   },
-  clickCellAt: ({wrapper, index}) => wrapper.find('[data-hook="cell"]').at(index).simulate('click')
+  clickCellAt: ({wrapper, index}) => wrapper.find('[data-hook="cell"]').at(index).simulate('click'),
+  isCelRevealedAt: ({wrapper, index}) => wrapper.find('[data-hook="cell"]').at(index).hasClass('revealed')
 };
 describe('Game', () => {
   let wrapper;
@@ -44,5 +47,26 @@ describe('Game', () => {
     driver.clickCellAt({wrapper, index: 0});
     driver.saveGame({wrapper, gameName: gameExpectedData.name});
     return eventually(() => expect(savedData).to.eql(gameExpectedData));
+  });
+
+  it('should load a saved game', () => {
+    const savedGame = {
+      name: 'Yaniv',
+      board: {
+        [JSON.stringify({x: 0, y: 0})]: {revealed: true}}
+    };
+
+    nock(getTestBaseUrl()).get(`/api/game/${savedGame.name}`).reply(() => savedGame);
+
+    wrapper = mount(
+      <MemoryRouter initialEntries={[`/${savedGame.name}`]}>
+        <Route path="/:gameName" component={Game}/>
+      </MemoryRouter>,
+       {attachTo: document.createElement('div')}
+    );
+
+    return eventually(() => {
+      expect(driver.isCelRevealedAt({wrapper, index: 0})).to.eql(true);
+    });
   });
 });
