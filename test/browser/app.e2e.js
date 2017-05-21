a1c2c74e35bb294d2c6f71c28d6dc729e03e9a90
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-05-21 14:26:39 +0300

    add a red e2e for loading a saved game

--- test/browser/app.e2e.js
+++ test/browser/app.e2e.js
@@ -2,26 +2,36 @@ import {expect} from 'chai';
 import 'babel-polyfill';
 import {beforeAndAfter, browserAfterEach} from '../environment';
 
-const appDriver = {
-  navigate: (url = '/') => browser.get(url),
-  clickNewGame: () => $(`[data-hook=new-game]`).click(),
-  getSavedGames: () => $$(`[data-hook=game]`).map(el => el.getText())
+const appDriverFactory = () => {
+  const games = $$(`[data-hook=game]`);
+  return {
+    navigate: (url = '/') => browser.get(url),
+    clickNewGame: () => $(`[data-hook=new-game]`).click(),
+    getSavedGames: () => games.map(el => el.getText()),
+    loadAGameAt: index => games.get(index).click()
+  };
 };
 
-const gameDriverFactory = () => ({
-  getRows: () => $$('[data-hook=row]'),
-  getCells: () => $$('[data-hook=cell]'),
-  saveGame: async game => {
-    await $('[data-hook=game-name-input]').clear().sendKeys(game.name);
-    await $('[data-hook=game-save]').click();
-  }
-});
+const gameDriverFactory = () => {
+  const cells = $$('[data-hook=cell]');
+  return {
+    getRows: () => $$('[data-hook=row]'),
+    getCells: () => cells,
+    saveGame: async game => {
+      await $('[data-hook=game-name-input]').clear().sendKeys(game.name);
+      await $('[data-hook=game-save]').click();
+    },
+    clickACellAt: index => cells.get(index).click(),
+    getCellAt: index => cells.get(index)
+  };
+};
 
 describe('React application', () => {
   beforeAndAfter();
   browserAfterEach();
 
   const gameDriver = gameDriverFactory();
+  const appDriver = appDriverFactory();
 
   it('should create a new game', async () => {
     await appDriver.navigate();
@@ -37,5 +47,16 @@ describe('React application', () => {
     await gameDriver.saveGame(game);
     await appDriver.navigate();
     expect(await appDriver.getSavedGames()).to.eql([game.name]);
+  });
+
+  it('should load a saved game', async () => {
+    const game = {name: 'yaniv'};
+    await appDriver.navigate();
+    await appDriver.clickNewGame();
+    await gameDriver.clickACellAt(0);
+    await gameDriver.saveGame(game);
+    await appDriver.navigate();
+    await appDriver.loadAGameAt(0);
+    expect(await gameDriver.getCellAt(0).getAttribute('class')).to.contain('revealed');
   });
 });
